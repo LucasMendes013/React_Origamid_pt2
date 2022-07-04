a2917243478e3ac020627e2ba5633972b418213d
LucasMendes013 <104436533+LucasMendes013@users.noreply.github.com>
2022-07-04 11:23:05 -0300

    JSX1

--- meuapp/src/App.js
+++ meuapp/src/App.js
@@ -1,7 +1,24 @@
 import React from "react";
 
 const App = () => {
-  return <div>Meu novo app React </div>
+  const nome = 'Lucas'
+  const estilo = {
+    color: 'blue',
+    fontSize: '30px',
+    fontFamily: 'Helvetica',
+    padding: '20px',
+    textAlign: 'center',
+    maxWidth: '2000px',
+  }
+  return  (
+    <>
+      <div className="divisoria" style={estilo}>Olá Mundo</div>
+      <label htmlFor="nome">Nome {nome}</label>
+      <input type="text" id="nome"></input>
+    </>
+
+          )
+  
 }
 
 export default App
